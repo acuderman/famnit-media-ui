a7385a3e3d6279660be993b24bf24a608103df03
Andraž Cuderman <andraz.cuderman@globalid.net>
2019-12-29 22:45:16 +0100

    authentication with server

--- src/helpers/authentication.js
+++ src/helpers/authentication.js
@@ -2,12 +2,14 @@ import * as axios from 'axios'
 import { API_BASE_URI } from '../config'
 
 export async function getAccessToken(id_token, youtube_access_token) {
-    return axios.post(`${API_BASE_URI}/auth/token`, {
+    const response = await axios.post(`${API_BASE_URI}/auth/token`, {
         id_token,
         youtube_access_token,
     })
+
+    return response.data.access_token
 }
 
 export async function verifyAccessToken(token) {
-    return axios.post(`${API_BASE_URI}/auth/verify`, { headers: `Bearer ${token}`})
+    return axios.post(`${API_BASE_URI}/auth/verify`,{}, { headers: { authorization: `Bearer ${token}` }})
 }
