248bd5ef8d5638a8145a3a08db4aa16dc51cf335
Andraž Cuderman <andraz.cuderman@globalid.net>
2020-01-07 18:20:38 +0100

    f

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// read from .env
+
+export const API_BASE_URI = 'https://famnit-tutorial.herokuapp.com'
+export const GOOGLE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3'
+export const GOOGLE_AUTH_API_BASE_URL = 'https://www.googleapis.com/oauth2/v3'
+export const YOUTUBE_API_KEY = 'AIzaSyB0gGQJXYtb6tOOzqMdH_oQ4vVLc5Nct5U'
+
+export const BASE_URL = 'http://localhost:3000'
