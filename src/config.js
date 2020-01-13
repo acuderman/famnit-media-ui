1d273d3063df496459af206a20f40aef706ce4a2
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-13 15:00:27 +0100

    fix base url

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const GOOGLE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3'
 export const GOOGLE_AUTH_API_BASE_URL = 'https://www.googleapis.com/oauth2/v3'
 export const YOUTUBE_API_KEY = 'AIzaSyB0gGQJXYtb6tOOzqMdH_oQ4vVLc5Nct5U'
 
-export const BASE_URL = window.location.href.indexOf('localhost') !== -1 ? 'http://localhost:3000' : 'https://famnit-media-tutorials.herokuapp.com/'
+export const BASE_URL = window.location.href.indexOf('localhost') !== -1 ? 'http://localhost:3000' : 'https://famnit-media-tutorials.herokuapp.com'
