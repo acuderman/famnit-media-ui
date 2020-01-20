9485949bb337043f715b543f21e881f533784f04
Andraz Cuderman <cuderman.andraz@gmail.com>
2020-01-20 22:24:04 +0100

    move yt API key to env variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const API_BASE_URI = 'https://famnit-media.herokuapp.com'
 export const GOOGLE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3'
 export const GOOGLE_AUTH_API_BASE_URL = 'https://www.googleapis.com/oauth2/v3'
-export const YOUTUBE_API_KEY = 'AIzaSyB0gGQJXYtb6tOOzqMdH_oQ4vVLc5Nct5U'
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY
 
-export const BASE_URL = window.location.href.indexOf('localhost') !== -1 ? 'http://localhost:3000' : 'https://famnit-media-tutorials.herokuapp.com'
+export const BASE_URL = process.env.REACT_APP_SITE_URL || 'http://localhost:3000'
